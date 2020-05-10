458953b825ef7f419b6f1fa4edea2fac7ac25ec7
matt8poi <matt8poi@gmail.com>
2020-05-10 17:39:25 -0600

    Used fragment as root element.

--- src/index.js
+++ src/index.js
@@ -16,11 +16,11 @@ const App = () => {
   const age = 10
 
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
-    </div>
+    </>
   )
 }
 
